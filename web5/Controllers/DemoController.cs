a593fe0fffab9e118f7f83af3b982345b2f89ba6
MinhBui2002 <heribertschneller@gmail.com>
2022-07-26 10:33:20 +0700

    fafefwa

--- web5/Controllers/DemoController.cs
+++ web5/Controllers/DemoController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using web5.Models;
 
 namespace web5.Controllers
 {
@@ -6,7 +8,51 @@ namespace web5.Controllers
     {
         public IActionResult Index()
         {
+            return View("~/Views/Demo/Greenwich.cshtml");
+        }
+
+        public IActionResult Demo1()
+        {
+            string City = "London";
+            int Population = 8_000_000;
+            bool isChecked = true;
+            double Temperature = 1.8;
+            ViewData["City"] = City;
+            ViewData["Population"] = Population;
+            ViewData["isChecked"] = isChecked;
+            ViewData["Temperature"] = Temperature;
+
+
             return View();
         }
+
+        public IActionResult Demo2()
+        {
+            List<string> Cities = new List<string>()
+            {
+                "London",
+                "Paris",
+                "New York",
+                "Tokyo",
+                "Beijing",
+                "Moscow",
+                "Seoul",
+            };
+            return View();
+        }
+
+        public IActionResult Demo3()
+        {
+            //create a list of mobiles
+            List<Mobile> mobiles = new List<Mobile>();
+            mobiles.Add(new Mobile(1, "Samsung", 32, 324.3));
+            mobiles.Add(new Mobile(2, "Apple", 32, 324.3));
+            mobiles.Add(new Mobile(3, "OnePlus", 32, 324.3));
+            
+
+
+            return View(mobiles);
+        }
+
     }
 }
