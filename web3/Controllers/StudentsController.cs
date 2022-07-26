7fe0fe9532c1b9a59aeffe3d7eec8520ece6a967
MinhBui2002 <heribertschneller@gmail.com>
2022-07-26 08:08:58 +0700

    dq

---
+++ web3/Controllers/StudentsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using web3.Models;
+
+namespace web3.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            Student s1 = new Student(1, "John", 20, "123-456-7890");
+            Student s2 = new Student(2, "Jane", 21, "123-456-7891");
+            Student s3 = new Student(3, "Jack", 22, "123-456-7892");
+            Student s4 = new Student(4, "Jill", 23, "123-456-7893");
+            
+
+            //create a list of students 
+            List<Student> students = new List<Student>();
+            students.Add(s1);
+            students.Add(s2);
+            students.Add(s3);
+            students.Add(s4);
+
+
+            return View(students);
+        }
+    }
+}
---
+++ web3/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace web3.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Student(int Id, string Name, int Age, string PhoneNumber)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Age = Age;
+            this.PhoneNumber = PhoneNumber;
+        }
+        
+
+    }
+}
